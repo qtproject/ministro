06933161d9c7c5ef0921b00568c24ff442ae7ae3
Ray Donnelly <mingw.android@laptop.com>
2011-07-09 19:54:11 +0100

    Use GDB 7.3.50.20110709 (branch fsf_head)

--- Necessitas_SDK/ndk_vars.sh
+++ Necessitas_SDK/ndk_vars.sh
@@ -2,10 +2,10 @@ NDK_VER=r5c
 
 # Refers to the gdb branches and folder structure on
 # git://gitorious.org/toolchain-mingw-android/mingw-android-toolchain-gdb.git
-GDB_BRANCH=integration_7_3
+GDB_BRANCH=fsf_head
 GDB_ROOT_PATH=
 # This is the name given to the created package.
-GDB_VER=7.3
+GDB_VER=7.3.50.20110709
 #GDB_BRANCH=master
 #GDB_ROOT_PATH=gdb
 #GDB_VER=7.3
